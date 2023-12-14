c10dc9399136b5fbb1d3af87225c44737c032334
NicasioMunyi <nickalfred200@gmail.com>
2023-12-14 14:17:54 +0300

    login page

---
+++ admin/adminLogin.php
@@ -0,0 +1,97 @@
+<?php
+
+session_start();
+
+include_once("../database/config.php");
+
+//check if admin is already logged in
+
+if(isset($_SESSION['admin_id'])){
+    header("Location: index.php");
+    exit();
+}
+
+//check if form submitted
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    //input validation
+
+    if(empty($username) || empty($password)){
+        $error ="Username and Password Required";
+
+    }else{
+        //check login credentials
+        $stmt = $conn->prepare("SELECT AdminID,Password FROM Admins WHERE Username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $stmt->store_results();
+
+        if($stmt->num_rows > 0){
+            $stmt->bind_result($AdminID ,$hashedPassword);
+            $stmt->fetch();
+
+            
+            //verify password
+            if(password_verify($password,$hashedPassword)){
+                $_SESSION['admin_id'] = $AdminID;
+                header("Location: index.php");
+                exit();
+            }else{
+                $error = "Invalid Password";
+
+            
+
+
+        }}else{
+                $error = "Invalid Username";
+            }
+
+        $stmt->close();
+
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/index.css">
+    <link rel="stylesheet" href="../assets/css/form.css">
+    <link rel="stylesheet" href="../assets/css/footer.css">
+
+    <title>Admin Login</title>
+</head>
+<body>
+
+<div>
+        <div class="container">
+            
+            <?php if (isset($error)){ ?>
+                <p class ="error"><?php echo $error; ?><p>        
+
+            <?php } ?>
+
+            <form method ="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <h2>Admin Login</h2>
+                <div class="input">
+                    <input type="text" id="username" placeholder="Username" name="username" required>
+                </div>
+                <div class="input">
+                    <input type="password" id="password" placeholder="Passoword" name="password" required>
+                    <div class="forgot">
+                        <a href="#">Forgot Paasword?</a>
+                    </div>
+                </div>
+
+                <button type="submit">Login</button>
+            </form>
+            
+
+        </div>
+    <?php include_once("../includes/footer.php"); ?>
+    
---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+</div>
+<div class = "footer">
+    <p> &copy; <?php echo date("Y"); ?> Library Management System </P>
+
+</div>
+</body>
+</html>
